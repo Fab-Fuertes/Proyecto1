473d070ab2e87d0e57892fc57c95aeaed21d93c3
Fab-Fuertes <fabiana.fuertes@correo.unimet.edu.ve>
2023-10-03 15:08:24 -0400

    Cambios de Arturo y Carlos (Sin el ordenamiento de archivos por paquetes).

---
+++ src/proyecto1/MostrarGrafo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+s
+
+/**
+ *
+ * @author fabys
+ */
+public class MostrarGrafo {
+    
+    private static final String NOMBRE_GRAFO = "Grafo de usuarios y sus relaciones";
+
+    public static void main(String[] args) {
+
+        Graph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
+
+        graph.addVertex("Alice");
+        graph.addVertex("Bob");
+        graph.addVertex("Carol");
+        graph.addVertex("Dave");
+
+        graph.addEdge("Alice", "Bob");
+        graph.addEdge("Bob", "Carol");
+        graph.addEdge("Carol", "Dave");
+        graph.addEdge("Dave", "Alice");
+
+        graph.removeVertex("Alice");
+
+        System.out.println(NOMBRE_GRAFO + ": " + graph.toString());
+    }
+    
+}
